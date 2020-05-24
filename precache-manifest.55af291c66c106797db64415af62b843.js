56b9190817e7c24c0bc87ea8eb8e28324ec1a12c
mma judith <mmajudith2000@gmail.com>
2020-05-24 10:28:26 -0700

    Updates

---
+++ precache-manifest.55af291c66c106797db64415af62b843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45925cf0f4d9a666c277c757bef8b07",
+    "url": "/rest-countries-api/index.html"
+  },
+  {
+    "revision": "fe1b5ac9b70067173082",
+    "url": "/rest-countries-api/static/css/main.ca788566.chunk.css"
+  },
+  {
+    "revision": "6870756aba0fdf844d65",
+    "url": "/rest-countries-api/static/js/2.db8d4351.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rest-countries-api/static/js/2.db8d4351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1b5ac9b70067173082",
+    "url": "/rest-countries-api/static/js/main.52301d17.chunk.js"
+  },
+  {
+    "revision": "fbd2ad259358bb1e7e2c",
+    "url": "/rest-countries-api/static/js/runtime-main.f71b98d8.js"
+  }
+]);
